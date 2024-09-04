89341cd23edf6dcf0c89db9b1442a5b3282be4c2
Valentino Lugli <valentinolugli@gmail.com>
2024-09-04 22:05:57 +0200

    feat: Adding script to execute train/test on NGPU

---
+++ scripts/ngpu/train_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Job name        |        |
+#SBATCH --job-name=PS-TRAIN
+
+# Assign job to a queue
+#SBATCH --partition=dgx
+
+# Use GPU
+#SBATCH --gres=gpu:1
+
+# Assign job to a particular node
+##SBATCH --nodelist=talos
+
+# Default configs for NGPU
+export PATH="/opt/anaconda/anaconda3/bin:$PATH"
+export PATH="/opt/anaconda/bin:$PATH"
+eval "$(conda shell.bash hook)"
+export TFHUB_CACHE_DIR=.
+
+# Activating Python enviroment.
+source ./env/bin/activate
+# Calling the script passing all parameters from outside.
+python ./train_test.py $*

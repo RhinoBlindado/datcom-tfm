f42cbcab6d055df6894ec49207e7916261f936c8
Valentino Lugli <valentinolugli@gmail.com>
2024-09-18 19:13:38 +0200

    feat: Added SLURM script for optimized training

---
+++ scripts/ngpu/optimized_train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Job name        |        |
+#SBATCH --job-name=OP-TRAIN
+
+# Assign job to a queue
+#SBATCH --partition=dgx,dgx2
+
+# Use GPU
+#SBATCH --gres=gpu:1
+
+# Assign job to a particular node
+##SBATCH --nodelist=talos
+
+# Default configs for NGPU
+export PATH="/opt/anaconda/anaconda3/bin:$PATH"
+export PATH="/opt/anaconda/bin:$PATH"
+eval "$(conda shell.bash hook)"
+export TFHUB_CACHE_DIR=.
+
+# Activating Python enviroment.
+source ./env/bin/activate
+# Calling the script passing all parameters from outside.
+python ./optimized_train.py $*

7d14361dddbf6c097e7944bfc1113e2f44e56385
Valentino Lugli <valentinolugli@gmail.com>
2024-10-29 23:05:39 +0100

    fix: Use absolute path for softlinks

--- scripts/misc/create_softlinks.py
+++ scripts/misc/create_softlinks.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dst-folder", type=str)
 parser.add_argument("--src-folder", type=str)
 parser.add_argument("--pattern", choices=["L", "Rm", "R"])
+parser.add_argument("--force", action="store_true")
 
 args = parser.parse_args()
 
@@ -17,5 +18,9 @@ pattern_files = glob.glob(os.path.join(args.src_folder, f"*-{args.pattern}.npy")
 
 for f_path in pattern_files:
     file = os.path.split(f_path)[1]
-    os.symlink(f_path, os.path.join(args.dst_folder, file))
+
+    if os.path.islink(os.path.join(args.dst_folder, file)) and args.force:
+        os.remove(os.path.join(args.dst_folder, file))
+
+    os.symlink(os.path.abspath(f_path), os.path.join(args.dst_folder, file))
     print(f"Linking {f_path} -> {os.path.join(args.dst_folder, file)}")
